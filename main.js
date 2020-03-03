f4910520a0afca58951543c7a3d7fd52b9fca89c
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-03 12:26:19 +0100

    update 2

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 document.body.style.height = 10000 + "px";
+
+
+const div = document.createElement("div");
+
+document.body.appendChild(div);
+
+let size = 100 + "px";
+
+div.style.hight = size + "%";
+div.style.width = 100 + "%";
+div.style.position = "fixed";
+div.style.top = "0";
+div.style.left = "0";
+div.style.backgroundColor = "green";
+
+function changeHeight() {
+
+}
+
+document.addEventListener("scrol", changeHeight)
+
